835f3b5239232be3ccafa8c9a7d8ebe90cd4c340
Steven <stevenmb1995@gmail.com>
2024-06-19 21:06:22 -0500

    set up authentication with firebase

---
+++ app/src/main/java/com/example/firebaselogin/Login.java
@@ -0,0 +1,105 @@
+package com.example.firebaselogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+    EditText edtEmail,edtPassword;
+    Button btnLogin;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
+    TextView registerNow;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edtEmail=findViewById(R.id.email);
+        edtPassword=findViewById(R.id.password);
+        btnLogin=findViewById(R.id.btnLogin);
+        progressBar=findViewById(R.id.progressBar);
+        registerNow=findViewById(R.id.registerNow);
+        mAuth=FirebaseAuth.getInstance();
+
+        registerNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email,password;
+                email=edtEmail.getText().toString();
+                password=edtPassword.getText().toString();
+
+
+                if(email.isEmpty()){
+                    Toast.makeText(Login.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.isEmpty()){
+                    Toast.makeText(Login.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(Login.this, "Login Successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/firebaselogin/Register.java
@@ -0,0 +1,115 @@
+package com.example.firebaselogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import kotlin.text.RegexKt;
+
+public class Register extends AppCompatActivity {
+
+    EditText edtEmail,edtPassword;
+    Button btnRegister;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView loginNow;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        edtEmail=findViewById(R.id.email);
+        edtPassword=findViewById(R.id.password);
+        btnRegister=findViewById(R.id.btnRegister);
+        progressBar=findViewById(R.id.progressBar);
+        loginNow=findViewById(R.id.loginNow);
+        mAuth=FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email,password;
+                email=edtEmail.getText().toString();
+                password=edtPassword.getText().toString();
+            }
+        });
+
+        loginNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email=edtEmail.getText().toString();
+                password=edtPassword.getText().toString();
+
+
+                if(email.isEmpty()){
+                    Toast.makeText(Register.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.isEmpty()){
+                    Toast.makeText(Register.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(Register.this, "Account Created.",
+                                            Toast.LENGTH_SHORT).show();
+                                            Intent intent=new Intent(getApplicationContext(),Login.class);
+                                            startActivity(intent);
+                                            finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
