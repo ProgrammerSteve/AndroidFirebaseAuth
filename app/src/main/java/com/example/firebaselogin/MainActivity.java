b0fce19cf7e2375ac3056e409335a96d60edce2a
Steven <stevenmb1995@gmail.com>
2024-07-16 19:50:41 -0500

    added firestore to app

--- app/src/main/java/com/example/firebaselogin/MainActivity.java
+++ app/src/main/java/com/example/firebaselogin/MainActivity.java
@@ -1,22 +1,84 @@
 package com.example.firebaselogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser user;
     Button btnLogout;
-    TextView textView;
+    TextView userName;
+
+
+    EditText detailOneEdt;
+    TextView detailOneTxt;
+    Button detailOneBtn;
+
+    EditText detailTwoEdt;
+    TextView detailTwoTxt;
+    Button detailTwoBtn;
+
+    EditText detailThreeEdt;
+    TextView detailThreeTxt;
+    Button detailThreeBtn;
+
+    FirebaseFirestore db;
+
+
+
+    private boolean detailOneToggle=false;
+    private boolean detailTwoToggle=false;
+    private boolean detailThreeToggle=false;
+
+    private String detailOneFirestore;
+    private String detailTwoFirestore;
+    private String detailThreeFirestore;
+
+    public String getDetailOneFirestore() {
+        return detailOneFirestore;
+    }
+
+    public void setDetailOneFirestore(String detailOneFirestore) {
+        this.detailOneFirestore = detailOneFirestore;
+    }
+
+    public String getDetailTwoFirestore() {
+        return detailTwoFirestore;
+    }
+
+    public void setDetailTwoFirestore(String detailTwoFirestore) {
+        this.detailTwoFirestore = detailTwoFirestore;
+    }
+
+    public String getDetailThreeFirestore() {
+        return detailThreeFirestore;
+    }
+
+    public void setDetailThreeFirestore(String detailThreeFirestore) {
+        this.detailThreeFirestore = detailThreeFirestore;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +86,164 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth=FirebaseAuth.getInstance();
-        btnLogout=findViewById(R.id.btnLogout);
-        textView=findViewById(R.id.userDetails);
         user=mAuth.getCurrentUser();
-
+        userName=findViewById(R.id.userName);
+        detailOneBtn=findViewById(R.id.detailOneBtn);
+        detailTwoBtn=findViewById(R.id.detailTwoBtn);
+        detailThreeBtn=findViewById(R.id.detailThreeBtn);
         if (user==null){
             Intent intent=new Intent(getApplicationContext(),Login.class);
             startActivity(intent);
             finish();
         }else{
-            textView.setText(user.getEmail());
+            userName.setText(user.getEmail());
         }
+        db = FirebaseFirestore.getInstance();
+        btnLogout=findViewById(R.id.btnLogout);
 
+        detailOneEdt=findViewById(R.id.detailOneEdt);
+        detailTwoEdt=findViewById(R.id.detailTwoEdt);
+        detailThreeEdt=findViewById(R.id.detailThreeEdt);
+
+        detailOneTxt=findViewById(R.id.detailOneTxt);
+        detailTwoTxt=findViewById(R.id.detailTwoTxt);
+        detailThreeTxt=findViewById(R.id.detailThreeTxt);
+
+
+        CollectionReference users=db.collection("users");
+
+
+        if(db!=null){
+            String uid=user.getUid();
+            DocumentReference userDocRef=users.document(uid);
+
+            detailOneBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!detailOneToggle){
+                        detailOneToggle=true;
+                        detailOneEdt.setVisibility(View.VISIBLE);
+                        detailOneTxt.setVisibility(View.GONE);
+                        detailOneBtn.setText("Set");
+                        detailOneEdt.setText(getDetailOneFirestore());
+                    }else{
+                        detailOneToggle=false;
+                        detailOneEdt.setVisibility(View.GONE);
+                        detailOneTxt.setVisibility(View.VISIBLE);
+                        detailOneBtn.setText("Edit");
+                        Map<String,Object> detailOneObj=new HashMap<>();
+                        detailOneObj.put("detailOne",detailOneEdt.getText().toString());
+                        userDocRef.update(detailOneObj).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    setDetailOneFirestore(detailOneEdt.getText().toString());
+                                    detailOneTxt.setText(detailOneEdt.getText().toString());
+                                }else{
+                                    Toast.makeText(MainActivity.this, "Error updating", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                        });
+                    }
+                }
+            });
+            detailTwoBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!detailTwoToggle){
+                        detailTwoToggle=true;
+                        detailTwoEdt.setVisibility(View.VISIBLE);
+                        detailTwoTxt.setVisibility(View.GONE);
+                        detailTwoBtn.setText("Set");
+                        detailTwoEdt.setText(getDetailTwoFirestore());
+                    }else{
+                        detailTwoToggle=false;
+                        detailTwoEdt.setVisibility(View.GONE);
+                        detailTwoTxt.setVisibility(View.VISIBLE);
+                        detailTwoBtn.setText("Edit");
+                        Map<String,Object> detailTwoObj=new HashMap<>();
+                        detailTwoObj.put("detailTwo",detailTwoEdt.getText().toString());
+                        userDocRef.update(detailTwoObj).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    setDetailTwoFirestore(detailTwoEdt.getText().toString());
+                                    detailTwoTxt.setText(detailTwoEdt.getText().toString());
+                                }else{
+                                    Toast.makeText(MainActivity.this, "Error updating", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                        });
+                    }
+                }
+            });
+            detailThreeBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!detailThreeToggle){
+                        detailThreeToggle=true;
+                        detailThreeEdt.setVisibility(View.VISIBLE);
+                        detailThreeTxt.setVisibility(View.GONE);
+                        detailThreeBtn.setText("Set");
+                        detailThreeEdt.setText(getDetailThreeFirestore());
+                    }else{
+                        detailThreeToggle=false;
+                        detailThreeEdt.setVisibility(View.GONE);
+                        detailThreeTxt.setVisibility(View.VISIBLE);
+                        detailThreeBtn.setText("Edit");
+                        Map<String,Object> detailThreeObj=new HashMap<>();
+                        detailThreeObj.put("detailThree",detailThreeEdt.getText().toString());
+                        userDocRef.update(detailThreeObj).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    setDetailThreeFirestore(detailThreeEdt.getText().toString());
+                                    detailThreeTxt.setText(detailThreeEdt.getText().toString());
+                                }else{
+                                    Toast.makeText(MainActivity.this, "Error updating", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                        });
+                    }
+                }
+            });
+
+
+            userDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+
+                        if (document.exists()) {
+                            setDetailOneFirestore(document.getData().get("detailOne").toString());
+                            detailOneTxt.setText((getDetailOneFirestore().isEmpty()?"Not Set":getDetailOneFirestore()));
+                            detailOneEdt.setText(detailOneFirestore);
+
+                            setDetailTwoFirestore(document.getData().get("detailTwo").toString());
+                            detailTwoTxt.setText((getDetailTwoFirestore().isEmpty()?"Not Set":getDetailTwoFirestore()));
+                            detailTwoEdt.setText(detailTwoFirestore);
+
+                            setDetailThreeFirestore(document.getData().get("detailThree").toString());
+                            detailThreeTxt.setText((getDetailThreeFirestore().isEmpty()?"Not Set":getDetailThreeFirestore()));
+                            detailThreeEdt.setText(detailThreeFirestore);
+
+
+                        } else {
+                            Log.d("FIRESTORE", "No such document");
+                        }
+                    } else {
+                        Log.d("FIRESTORE", "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,14 +254,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        if(currentUser != null){
-//            reload();
-//        }
-//    }
 }
--- app/src/main/java/com/example/firebaselogin/Register.java
+++ app/src/main/java/com/example/firebaselogin/Register.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,14 +14,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import kotlin.text.RegexKt;
 
 public class Register extends AppCompatActivity {
+
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     EditText edtEmail,edtPassword;
     Button btnRegister;
@@ -95,12 +106,37 @@ public class Register extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
+                                    FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                                    if(firebaseUser!=null){
+                                        String uid = firebaseUser.getUid();
 
-                                    Toast.makeText(Register.this, "Account Created.",
-                                            Toast.LENGTH_SHORT).show();
-                                            Intent intent=new Intent(getApplicationContext(),Login.class);
-                                            startActivity(intent);
-                                            finish();
+                                        Map<String, Object> user = new HashMap<>();
+                                        user.put("detailOne", "");
+                                        user.put("detailTwo", "");
+                                        user.put("detailThree", "");
+
+                                        db.collection("users").document(uid)
+                                                .set(user)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Log.d("FIRESTORE", "DocumentSnapshot added with ID: " + uid);
+                                                        Toast.makeText(Register.this, "Account Created.",
+                                                                Toast.LENGTH_SHORT).show();
+                                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.w("FIRESTORE", "Error adding document", e);
+                                                        Toast.makeText(Register.this, "Failed to create user document.",
+                                                                Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Toast.makeText(Register.this, "Authentication failed.",
